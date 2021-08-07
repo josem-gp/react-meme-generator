786ec11c5d11db427a0d3e9e69f599b038b5c0f0
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-07 17:56:16 +0900

    connected parent App with child components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "./Header";
+import MemeGenerator from "./MemeGenerator";
 
 const App = () => {
-  return <div>hello!</div>;
+  return (
+    <div>
+      <Header />
+      <MemeGenerator />
+    </div>
+  );
 };
 
 export default App;
