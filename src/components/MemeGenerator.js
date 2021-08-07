583ed396ef2e0ea15de28cc8c3aaf8a5bc9fb990
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-07 18:07:54 +0900

    initialized three states for MemeGenerator component

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MemeGenerator = () => {
+  const [topText, setTopText] = useState("");
+  const [bottomText, setBottomText] = useState("");
+  const [randomImg, setRandomImg] = useState("http://i.imgflip.com/1bij.jpg");
   return (
     <div>
       <p>Hi from MemeGenerator!</p>
