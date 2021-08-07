c3f92101b2c189175602c5a4dd26089cc4c9098c
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-07 19:20:34 +0900

    adding effect for API call

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const MemeGenerator = () => {
   const [topText, setTopText] = useState("");
   const [bottomText, setBottomText] = useState("");
   const [randomImg, setRandomImg] = useState("http://i.imgflip.com/1bij.jpg");
+  const [allMemeImgs, setAllMemeImgs] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((result) => result.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <div>
       <p>Hi from MemeGenerator!</p>
