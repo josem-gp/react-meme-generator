c473552fb65aa31ffcdf923b9729e55094163a35
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-07 17:59:40 +0900

    finished Header and styled it

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,13 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Hi from header!</h1>
-    </div>
+    <header>
+      <img
+        src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+        alt="Problem?"
+      />
+      <p>Meme Generator</p>
+    </header>
   );
 };
 
